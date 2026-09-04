a38845fc432f14a64c9f402efe304956f76f1840
Adelore Lessard <on3iropolos@gmail.com>
2026-09-04 18:19:49 -0500

    Add disko LUKS+btrfs layout for nvme0n1

---
+++ hosts/stump/disko.nix
@@ -0,0 +1,50 @@
+{ ... }:
+{
+  disko.devices = {
+    disk.nvme0n1 = {
+      type = "disk";
+      device = "/dev/nvme0n1";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            size = "1G";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [ "fmask=0077" "dmask=0077" ];
+            };
+          };
+          crypted = {
+            size = "100%";
+            content = {
+              type = "luks";
+              name = "crypted";
+              allowDiscards = true;
+              content = {
+                type = "btrfs";
+                extraArgs = [ "-f" ];
+                subvolumes = {
+                  "@root" = {
+                    mountpoint = "/";
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                  };
+                  "@home" = {
+                    mountpoint = "/home";
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                  };
+                  "@nix" = {
+                    mountpoint = "/nix";
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
